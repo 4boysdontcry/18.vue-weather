a37dc3c51e4a9c377cad42e650200a6cb0fb263e
4boysdontcry <hgy02166@gmail.com>
2021-06-10 15:05:21 +0900

    vue-animation

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Daily from '../views/Daily.vue'
+import Days from '../views/Days.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/',
+    name: 'Daily',
+    component: Daily
+  },
+  {
+    path: '/days',
+    name: 'Days',
+    component: Days
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Days.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="days-wrapper">
+    DAYS
+  </div>
+</template>
+
+<script>
+export default {
+  nema: 'Days'
+}
+</script>
+
+<style lang="scss" scoped>
+.days-wrapper {
+  font-size: 10vh;
+}
+</style>
