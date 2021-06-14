9a6a211299e0a0da22e039a13fbba27a2354fb67
4boysdontcry <hgy02166@gmail.com>
2021-06-14 12:20:21 +0900

    lat, lon 데이터 불러오기

--- src/views/Daily.vue
+++ src/views/Daily.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="daily-wrapper">
     <h2 class="title-wrap">
-      날씨정보
+      날씨정보 <span>{{ cityName }}</span>
     </h2>
     <b-form-select class="city-select" v-model="selected" :options="options" size="lg" />
+    <div>
+      <button class="btn btn-primary" @click="onClick">현재 위치의 날씨정보 확인</button>
+    </div>
   </div>
 </template>
 
@@ -14,24 +17,52 @@ import axios from 'axios'
 export default {
   name: 'Daily',
   data() {
+    // 현재 컴포넌트에서 쓰일 변수를 등록하는 곳
     return{
       city: [],
-      selected: ''
+      selected: '',
     }
   },
+  props: ['data'],
   computed: {
-    options: function(){
+    // 다른 변수값이 변해 내 값이 변하는 것들은 computed에 등록
+    // 계산되어진 변수 -> 함수안의 값이 변해서 리턴값이 변하면 자신도 변한다.
+    options: function(){    // 변수선언 한거임
       const city = []
-      this.city.forEach(v => {
-        city.push({ text: v.name, value: {lat: v.lat, lon: v.lon}, disabled: (v.lat && v.lon) ? false : true }) 
+      this.city.forEach( (v, i) => {
+        let isCity = v.lat && v.lon;
+        if(i == 0) city.push({text: '도시를 선택하세요', value: '', disabled: true })
+        if(!isCity) city.push({ text: '------------', value: '', disabled: true })
+        city.push({
+          text: v.name, value: {lat: v.lat, lon: v.lon}, disabled: (v.lat && v.lon) ? false : true 
+        }) 
+        if(!isCity) city.push({ text: '------------', value: '', disabled: true })
       })
       return city
+    },
+    ...mapGetters(['GET_DAILY']),
+    cityName: function(){
+      return this.GET_DAILY ? this.GET_DAILY.name : ''
     }
   },
-  async created() {
-		this.$store.dispatch('ACT_DAILY', null)
-    const { data } = await axios.get('/json/city.json')
+  watch: {    // 내가 변해서 다른 값들을 변하게 하는 것은 watch에 등록
+    selected: function(v, ov){
+      // select 값이 변하면 날씨정보를 가져오는 로직
+      this.$store.dispatch('ACT_DAILY', v)
+    },
+    GET_DAILY: function (v, ov){
+      console.log(v)
+    }
+  },
+  async created() {   // 자신(DAILY.vue)이 실행될 때 한번만 실행함
+		this.$store.dispatch('ACT_DAILY', null)   // 현재 위치의 날씨 정보 가져오기
+    const { data } = await axios.get('/json/city.json')   // 도시정보(위치값, 이름) 가져오기
     this.city = data.city
+  },
+  methods: {
+    onClick() {
+      this.$store.dispatch('ACT_DAILY', null)
+    },
   }
 }
 </script>
